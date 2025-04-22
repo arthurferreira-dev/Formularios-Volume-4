58945de8b2727c0cdd56371c177646039e3ed1c9
Arthur Ferreira <tutudacosta996@gmail.com>
2025-04-22 19:04:38 -0300

    finish

--- Form02/cad.php
+++ Form02/cad.php
@@ -1,1 +1,12 @@
-<p>Tem nada aqui!</p>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="$php_errormsg">
+</head>
+<body>
+    <p>Tem nada aqui!</p>
+</body>
+</html>
